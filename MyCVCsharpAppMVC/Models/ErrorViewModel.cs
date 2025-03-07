9fa3fb4948b06452c36905d67e8e96dc23c5d601
UrviVaghasiya <vaghasiyaurvisha123@gmail.com>
2025-03-07 12:40:35 +0000

    create MVC app

---
+++ MyCVCsharpAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyCVCsharpAppMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
