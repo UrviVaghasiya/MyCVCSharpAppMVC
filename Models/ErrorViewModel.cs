e8da6ca8632201d1feaf322d6cee99160b2a9419
UrviVaghasiya <vaghasiyaurvisha123@gmail.com>
2025-03-07 18:53:27 +0000

    Add Functionality
    - add CV and Tax controller page and handle it

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyCVCSharpAppMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- MyCVCsharpAppMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MyCVCsharpAppMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
