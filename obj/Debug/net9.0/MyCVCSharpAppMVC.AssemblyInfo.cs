e2bd75105ad848b9ad69bfb8e0c4c138a2efd1dc
UrviVaghasiya <vaghasiyaurvisha123@gmail.com>
2025-03-11 10:59:33 +0000

    update Footer CSS

--- obj/Debug/net9.0/MyCVCSharpAppMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyCVCSharpAppMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCVCSharpAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5131bb2ddf6c7f973a8485f669e1db88b0afea12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4269c14fd716a3ad4c296b680dad79b5e824b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCVCSharpAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCVCSharpAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
